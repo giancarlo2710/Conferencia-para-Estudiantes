f48719d891d7c0d226f75ecc4bc8b1f170736cef
giancarlo2710 <a.giansepulveda@seoane.edu.pe>
2026-07-13 20:08:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+/* Funciones, procedimiento  */
+function despedir(){
+    alert('See you tomorrow')
+}
+
+/* Función suma */
+function suma(n1, n2){
+    n1=parseInt(n1);
+    n2=parseInt(n2);
+
+    //mostrar el resultado
+    document.getElementById("resultado").innerHTML=n1+n2;
+}
+
+/* Completar las 4 operaciones */
